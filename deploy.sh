242d8264168be3d5450ed291b0d21f8c401de3a4
Alen Vrečko <alen.vrecko@gmail.com>
2012-03-26 21:37:10 +0200

    Fixed filename typo for sha1 command.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ mkdir -p repository/com/typesafe/path-hole/1.0/
 cp core/target/path-hole-1.0.jar repository/com/typesafe/path-hole/1.0/path-hole-1.0.jar
 openssl sha1 repository/com/typesafe/path-hole/1.0/path-hole-1.0.jar | awk '{print $2}' > repository/com/typesafe/path-hole/1.0/path-hole-1.0.jar.sha1
 cp deploy.pom repository/com/typesafe/path-hole/1.0/path-hole-1.0.pom
-openssl sha1 repository/com/typesafe/path-hole/1.0/path-hole-1.0.jar | awk '{print $2}' >  repository/com/typesafe/path-hole/1.0/path-hole-1.0.pom.sha1
+openssl sha1 repository/com/typesafe/path-hole/1.0/path-hole-1.0.pom | awk '{print $2}' >  repository/com/typesafe/path-hole/1.0/path-hole-1.0.pom.sha1
